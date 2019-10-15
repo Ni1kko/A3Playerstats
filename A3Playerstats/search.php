0d14578fa2b280d79864cbec7e57d397d2daf246
Nikko Renolds <ni1kko@outlook.com>
2019-10-15 04:21:09 +0100

    Cleanup

--- A3Playerstats/search.php
+++ A3Playerstats/search.php
@@ -16,20 +16,22 @@
     require_once("globals.php");  
     $users = array();
     
-    /**check with pid**/  
+    //check with pid  
     $result = Database::Query('SELECT * FROM players WHERE pid=:pid', array(':pid'=>$searchKey))[0]; 
     if(count($result) > 0){
         array_push($users,$result);
     }  
         
-    /**check with name**/
-    $result = Database::Query('SELECT * FROM players WHERE name LIKE :name', array(':name'=> '%'.$searchKey.'%'))[0]; 
+    // check with name
+    $result = Database::Query('SELECT * FROM players WHERE name LIKE :name', array(':name'=> '%'.$searchKey.'%')); 
     if(count($result) > 0){
-        array_push($users,$result);
+        foreach($result as $user){
+            array_push($users,$user);
+        }
     }
         
-    /**check with plate**/
-    $result = Database::Query("SELECT * FROM vehicles WHERE plate Like '%:plate%'", array(':plate'=>$searchKey))[0]; 
+    // check with plate
+    $result = Database::Query("SELECT * FROM vehicles WHERE active='1' AND plate Like :plate", array(':plate'=> '%'.$searchKey.'%')); 
     if(count($result) > 0){ 
         $result2 = Database::Query('SELECT * FROM players WHERE pid=:pid', array(':pid'=>$result["pid"]))[0]; 
         if(count($result2) > 0){
@@ -37,13 +39,10 @@
         }  
     }
     
-    /**get result count**/
+    //Check result count
     if(count($users)){ 
         $_SESSION["searchKey"] = $searchKey;
         $_SESSION["users"] = $users;
-
-        //echo "Found a result for: ".$_SESSION["searchKey"]."<br>"; 
-        //print_r($_SESSION["users"]);
         header("Location: result.php");
     }
     else
@@ -51,5 +50,5 @@
         header("Location: index.html");
         exit;
     }
-  
+?>
   
